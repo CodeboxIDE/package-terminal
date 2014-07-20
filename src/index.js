b5c397aaef5e01286c7746a0b96fe8600c829328
Samy Pessé <samypesse@gmail.com>
2014-07-19 17:39:24 -0700

    Add commands to open existing terminal

--- src/index.js
+++ src/index.js
@@ -3,17 +3,39 @@ define([
     "less!src/stylesheets/main.less"
 ], function(TerminalTab) {
     var commands = codebox.require("core/commands");
+    var rpc = codebox.require("core/rpc");
+    var dialogs = codebox.require("utils/dialogs");
 
     commands.register({
         id: "terminal.open",
         title: "Terminal: Open",
+        shortcuts: [
+            "alt+t"
+        ],
         run: function(args, context) {
-            return codebox.tabs.add(TerminalTab, {
-                cmd: ""
-            }, {
+            return codebox.tabs.add(TerminalTab, args, {
                 type: "terminal",
                 title: "Terminal",
                 section: "terminal"
+            });
+        }
+    });
+
+    commands.register({
+        id: "terminal.open.existing",
+        title: "Terminal: Open Existing",
+        shortcuts: [
+            "alt+shift+t"
+        ],
+        run: function(args, context) {
+            return rpc.execute("terminal/list")
+            .then(function(terminals) {
+                return dialogs.list(terminals)
+            })
+            .then(function(terminal) {
+                return commands.run("terminal.open", {
+                    shellId: terminal.get("value")
+                })
             });
         }
     });
